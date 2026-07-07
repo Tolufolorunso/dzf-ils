a0ff697fe0588165f3e3d93cad649aa1dcbab942
tolufolorunso <tolufolorunso@yahoo.com>
2026-07-07 21:42:53 +0100

    feat(cohorts): sync cohort students with Google Sheets and stats public api

--- src/app/api/stats/route.js
+++ src/app/api/stats/route.js
@@ -69,10 +69,6 @@ export async function GET(request) {
           totalTeachers,
           totalTeachersSchools,
           totalPatronsSchools,
-          details: {
-            totalStudentPatronsSchools: new Set(uniqueStudentSchools).size,
-            uniqueTeacherSchoolsList: uniqueTeacherSchools,
-          }
         },
       },
       {
